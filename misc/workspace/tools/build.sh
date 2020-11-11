ec0cc34351931137c35d4e38494ad0f15d128c83
Max Kuznetsov <maks.kuznetsov@gmail.com>
2020-11-11 22:20:44 +0300

    tools: remove outputs before build

--- misc/workspace/tools/build.sh
+++ misc/workspace/tools/build.sh
@@ -4,5 +4,9 @@ TEXINPUTS="/latex/texaas:$(kpsepath tex)"
 export TEXINPUTS
 export TEXMFVAR="/latex/.cache"
 
+find /latex/texaas -name "*latexmkrc" -delete
+
 cd "/latex/texaas/$1" || exit 1
+rm -f output.pdf output.log
+
 latexmk -r /tools/latexmkrc.pl -pdf "$2"
--- misc/workspace/tools/clean.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-find /latex/texaas -name "*latexmkrc" -delete
