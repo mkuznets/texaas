53053e5e91e65cf5293602fe49d8d4a62bb92b9b
Max Kuznetsov <maks.kuznetsov@gmail.com>
2020-11-14 03:40:07 +0300

    tools: set timeout for latexmk

--- misc/workspace/tools/build.sh
+++ misc/workspace/tools/build.sh
@@ -7,6 +7,8 @@ if [ "$(id -u)" -eq "0" ]; then
   exit 1
 fi
 
+TIMEOUT=300 # seconds
+
 TEXINPUTS="/latex/texaas:$(kpsepath tex)"
 export TEXINPUTS
 export TEXMFVAR="/latex/.cache"
@@ -19,4 +21,4 @@ find /latex/texaas -name "*latexmkrc" -delete
 cd "/latex/texaas/$1" || exit 1
 rm -f output.pdf output.log
 
-latexmk -r /tools/latexmkrc.pl -pdf "$2" >latexmk.log 2>&1
+timeout -s KILL "$TIMEOUT" latexmk -r /tools/latexmkrc.pl -pdf "$2" >latexmk.log 2>&1
